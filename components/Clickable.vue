9dc7d18931bf1b3c7067a3fb9ad3320ebc6160af
rorycai <coiggahou@foxmail.com>
2024-05-15 17:24:59 +0800

    💄 Clickable component

---
+++ components/Clickable.vue
@@ -0,0 +1,50 @@
+<template>
+   <div class="clickable">
+      <div class="flex justify-start items-center">
+         <span :class="slots.icon ? 'mr-1' : ''">
+            <slot name="icon"></slot>
+         </span>
+         <span class="clickable_text" :style="{
+            color: props.color
+         }" :class="{
+            'font-semibold': !!props.bold,
+            'italic': !!props.italic
+         }">{{ props.text }}</span>
+      </div>
+   </div>
+</template>
+
+<script lang="ts" setup>
+interface ClickableProps {
+   href?: string;
+   text: string;
+   // icon?: string;
+   color?: string;
+   bold?: boolean;
+   italic?: boolean;
+}
+const props = withDefaults(defineProps<ClickableProps>(), {
+   bold: false,
+   italic: false,
+   href: '',
+});
+const slots = useSlots();
+
+</script>
+
+<style lang="scss" scoped>
+.clickable {
+   @apply 
+   rounded px-1 py-0 mx-1 cursor-pointer inline-block align-middle
+   opacity-80
+   bg-gray-200/50 hover:bg-gray-200/95
+   dark:bg-gray-100/15 dark:hover:bg-gray-100/25
+   ;
+
+   &_text {
+      @apply 
+      text-gray-600 text-base hover:text-gray-800 
+      dark:text-gray-300 dark:hover:text-gray-100;
+   }
+}
+</style>
