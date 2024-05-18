28e78de34af737df9f2f4a3a3866aea07c9ddbee
rorycai <coiggahou@foxmail.com>
2024-05-18 17:39:02 +0800

    💄 Change nav styles and remove origin navs

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,18 +1,22 @@
 <template>
    <nav class="select-none">
       <ul class="flex justify-center items-center gap-1 pb-4 pt-12">
-         <li v-for="link of originNavs" :key="link._path">
-            <NuxtLink class="nav_item" :to="link._path">
-               {{ link.title }}
+         <li>
+            <NuxtLink class="nav_item" to="/">
+               Me
             </NuxtLink>
          </li>
-         <NuxtLink class="nav_item" to="/blogs">
-            Blogs
-         </NuxtLink>
-         <a class="nav_item cursor-pointer" @click="openModal">
-            <SearchIcon id="nav_search_icon" class="h-5 w-5 mr-2" filled :font-controlled="false"
-               @mouseenter="handleMouseOverSearchIcon" @mouseleave="handleMouseOutSearchIcon" />
-         </a>
+         <li>
+            <NuxtLink class="nav_item" to="/blogs">
+               Blogs
+            </NuxtLink>
+         </li>
+         <li>
+            <a class="nav_item cursor-pointer" @click="openModal">
+               <SearchIcon id="nav_search_icon" class="h-5 w-5 mr-2" filled :font-controlled="false"
+                  @mouseenter="handleMouseOverSearchIcon" @mouseleave="handleMouseOutSearchIcon" />
+            </a>
+         </li>
       </ul>
    </nav>
 </template>
@@ -21,17 +25,6 @@
 import SearchIcon from "@/assets/icons/search.svg";
 import type { NavItem } from '@nuxt/content/types';
 const { openModal } = useGlobalSearchModal();
-
-const navSelectorQuery = {
-   where: [
-      // / No
-      // /xxxx Yes
-      // /xxx/xxx No
-      { _path: /^\/\w+$/ },
-   ],
-}
-
-const originNavs: NavItem[] = await fetchContentNavigation()
 
 const { $anime } = useNuxtApp()
 const handleMouseOverSearchIcon = () => {
@@ -55,11 +48,33 @@ const handleMouseOutSearchIcon = () => {
 .nav_item {
    //   border-b-2 border-b-transparent hover:border-gray-400
    font-family: 'Fira Sans Condensed' !important;
-   @apply text-xl text-gray-300 hover:text-gray-800 transition-all dark:text-gray-600 dark:hover:text-gray-300 py-2 my-1 mx-2 decoration-gray-200 dark:decoration-gray-700;
+   @apply text-xl text-gray-300 hover:text-gray-800 transition-all dark:text-gray-600 dark:hover:text-gray-300 py-2 my-1 mx-2;
+   // decoration-gray-200 dark:decoration-gray-700;
+
+   position: relative;
+
+   &::after {
+      content: '';
+      display: block;
+      position: absolute;
+      z-index: -1;
+      bottom: 8px;
+      left: 3px;
+      right: 3px;
+      height: 12px;
+      @apply bg-gray-200;
+      transition: width 0.3s;
+      opacity: 0;
+   }
 
    &.router-link-exact-active {
       //  @apply text-gray-800 dark:text-gray-300;
-      @apply text-gray-800 dark:text-gray-300 underline underline-offset-1 decoration-8 font-semibold;
+      @apply text-gray-800 dark:text-gray-300 font-semibold;
+
+      //  underline underline-offset-1 decoration-8 ;
+      &::after {
+         opacity: 1;
+      }
    }
 }
 </style>
