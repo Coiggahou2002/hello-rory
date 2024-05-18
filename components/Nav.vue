1d54ab430f1d9f592a2b3862fa62223bf0901018
rorycai <coiggahou@foxmail.com>
2024-05-18 23:25:27 +0800

    💄 Redesign nav style & search button animation

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,21 +1,22 @@
 <template>
-   <nav class="select-none">
-      <ul class="flex justify-center items-center gap-1 pb-4 pt-12">
-         <li>
+   <nav class="select-none w-full max-w-screen-md px-8 lg:px-0">
+      <ul class="flex justify-between items-center gap-1 pb-4 pt-12">
+         <li class="mr-4">
             <NuxtLink class="nav_item" to="/">
                Me
             </NuxtLink>
          </li>
-         <li>
+         <li class="">
             <NuxtLink class="nav_item" to="/blogs">
                Blogs
             </NuxtLink>
          </li>
-         <li>
-            <a class="nav_item cursor-pointer" @click="openModal">
-               <SearchIcon id="nav_search_icon" class="h-5 w-5 mr-2" filled :font-controlled="false"
-                  @mouseenter="handleMouseOverSearchIcon" @mouseleave="handleMouseOutSearchIcon" />
-            </a>
+         <span class="grow shrink-0"></span>
+         <li class="invisible lg:visible">
+            <div class="search_btn flex items-center justify-start" @click="dispatchOpenModal" ref="SearchBtnRef">
+               <SearchIcon id="nav_search_icon" class="h-4 w-4 mr-2" filled :font-controlled="false" />
+               <span>Search</span>
+            </div>
          </li>
       </ul>
    </nav>
@@ -26,29 +27,61 @@ import SearchIcon from "@/assets/icons/search.svg";
 import type { NavItem } from '@nuxt/content/types';
 const { openModal } = useGlobalSearchModal();
 
-const { $anime } = useNuxtApp()
-const handleMouseOverSearchIcon = () => {
+const SearchBtnRef = ref<HTMLLIElement | null>(null);
+const { $anime } = useNuxtApp();
+const dispatchOpenModal = () => {
+   // SearchBtnRef.value?.classList.add('zoomed-and-centered');
+   const [
+      originTop,
+      originLeft,
+   ] = [SearchBtnRef.value?.offsetTop, SearchBtnRef.value?.offsetLeft];
+   if (!SearchBtnRef.value) return;
+   const newNode = SearchBtnRef.value.cloneNode(true) as HTMLDivElement;
+   newNode.style.position = 'fixed';
+   newNode.style.top = `${originTop}px`;
+   newNode.style.left = `${originLeft}px`;
+   document.body.appendChild(newNode);
+   const [width, height] = [window.innerWidth / 2, SearchBtnRef.value.offsetHeight * 2]
+   const elTop = window.innerHeight * 0.2;
+   const elLeft = window.innerWidth / 2 - width / 2;
+   let openModalExecuted = false;
    $anime({
-      targets: '#nav_search_icon',
-      scale: 1.4,
-      easing: 'spring(1, 80, 10, 20)'
+      targets: newNode,
+      width: width,
+      height: height,
+      top: elTop,
+      left: elLeft,
+      easing: 'easeOutQuad',
+      duration: 200,
+      update: (anim) => {
+         if (openModalExecuted) return;
+         const { progress } = anim;
+         if (progress > 59) {
+            openModal();
+            openModalExecuted = true;
+         }
+      }
+   }).finished.then(() => {
+      // remove the anim el
+      newNode.remove();
    })
+}
 
-}
-const handleMouseOutSearchIcon = () => {
-   $anime({
-      targets: '#nav_search_icon',
-      scale: 1,
-      easing: 'spring(1, 80, 10, 1)'
-   })
-}
 </script>
 
 <style lang="scss" scoped>
+.search_btn {
+   @apply w-32 cursor-pointer py-2 px-2 bg-gray-100/75 dark:bg-gray-900/75 rounded-lg text-sm text-gray-400 hover:scale-110 transition-transform;
+
+   span {
+      @apply text-gray-400;
+   }
+}
+
 .nav_item {
    //   border-b-2 border-b-transparent hover:border-gray-400
    font-family: 'Fira Sans Condensed' !important;
-   @apply text-xl text-gray-300 hover:text-gray-800 transition-all dark:text-gray-600 dark:hover:text-gray-300 py-2 my-1 mx-2;
+   @apply text-xl text-gray-300 hover:text-gray-800 transition-all dark:text-gray-600 dark:hover:text-gray-300 py-2 my-1;
    // decoration-gray-200 dark:decoration-gray-700;
 
    position: relative;
@@ -61,8 +94,8 @@ const handleMouseOutSearchIcon = () => {
       bottom: 8px;
       left: 3px;
       right: 3px;
-      height: 12px;
-      @apply bg-gray-200;
+      height: 14px;
+      @apply bg-gray-200 dark:bg-gray-700;
       transition: width 0.3s;
       opacity: 0;
    }
