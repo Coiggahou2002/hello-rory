de64af2eae4e921cd1bb0d789abf82364cc71ea7
rorycai <coiggahou@foxmail.com>
2024-05-19 22:41:38 +0800

    💄 Override blockquote style

---
+++ components/content/ProseBlockquote.vue
@@ -0,0 +1,5 @@
+<template>
+   <blockquote class="bg-gray-100 dark:bg-gray-900/75 rounded p-2 border-l-4 border-gray-200 dark:border-gray-950 font-normal my-2 text-sm ">
+     <slot class="text-gray-300"/>
+   </blockquote>
+ </template>
