6d18c3db3670219d526d7f74b6c3fcbacabe6871
rorycai <coiggahou@foxmail.com>
2024-05-20 00:13:58 +0800

    💄 Custom selection color

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@ useGlobalSearchModal();
   <div class="flex flex-col justify-center items-center">
     <Nav />
     <ClientOnly>
-      <SearchModal/>
+      <SearchModal />
     </ClientOnly>
     <!-- <main class="w-full max-w-screen-lg"> -->
     <main class="w-full max-w-screen-md px-8 lg:px-0">
@@ -26,22 +26,28 @@ html {
   font-family: 'Barlow' !important;
   font-size: 18px;
   scroll-behavior: smooth;
-  @apply text-slate-700
+  @apply text-slate-700 selection:text-gray-800 selection:bg-gray-200 dark:selection:bg-gray-700 dark:selection:text-gray-200;
 }
 
 body {
-  @apply dark:bg-gray-800 bg-white;
+  @apply dark:bg-gray-800 bg-gray-50;
 }
 
-h1, h2, h3, h4, p, span {
-   /* @apply text-slate-600 dark:text-slate-300; */
-   @apply text-gray-600 dark:text-gray-300;
+h1,
+h2,
+h3,
+h4,
+p,
+span {
+  /* @apply text-slate-600 dark:text-slate-300; */
+  @apply text-gray-600 dark:text-gray-300;
 }
 
 .page-enter-active,
 .page-leave-active {
   transition: all 0.4s;
 }
+
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
