1c26c0bf14d58afd35d9be882120ce70f444e60d
rorycai <coiggahou@foxmail.com>
2024-05-30 11:54:30 +0800

    💄 Add bottom space for blogs page

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="w-full flex-col flex">
-    <ContentList path="/blogs" v-slot="{ list }">
-      <div v-for="article in list" :key="article._path">
-        <BlogLink :blog="article"/>
-      </div>
-    </ContentList>
+  <div>
+    <div class="w-full flex-col flex">
+      <ContentList path="/blogs" v-slot="{ list }">
+        <div v-for="article in list" :key="article._path">
+          <BlogLink :blog="article" />
+        </div>
+      </ContentList>
+    </div>
+    <div class="h-64 w-full bg-transparent"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
