1142c390fc829aa7e1c368f7b8abe7fcb20566b2
rorycai <coiggahou@foxmail.com>
2024-06-24 14:07:42 +0800

    💄 Remove hash tag from ProseH2 and ProseH3

--- components/content/ProseH2.vue
+++ components/content/ProseH2.vue
@@ -1,21 +1,19 @@
 <template>
-   <h2 class="font-semibold text-2xl mt-8 mb-4 text-gray-700 dark:text-white" :id="id">
-      <span class="mr-2 opacity-50">##</span>
-     <a
-       v-if="id && generate"
-       :href="`#${id}`"
-     >
-       <slot />
-     </a>
-     <slot v-else />
-   </h2>
- </template>
- 
- <script setup lang="ts">
- import { computed, useRuntimeConfig } from '#imports'
- 
- const props = defineProps<{ id?: string }>()
- 
- const { headings } = useRuntimeConfig().public.mdc
- const generate = computed(() => props.id && headings?.anchorLinks?.h2)
- </script>
+  <h2 class="group font-semibold text-2xl mt-8 mb-4 text-gray-700 dark:text-white flex items-center" :id="id">
+    <!-- <div class="mr-2 opacity-50 absolute -left-12">##</div> -->
+    <div class="ml-2 text-xl absolute -left-10 opacity-0 group-hover:opacity-15">H2</div>
+    <a v-if="id && generate" :href="`#${id}`" class="inline-block">
+      <slot />
+    </a>
+    <slot v-else />
+  </h2>
+</template>
+
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from '#imports'
+
+const props = defineProps<{ id?: string }>()
+
+const { headings } = useRuntimeConfig().public.mdc
+const generate = computed(() => props.id && headings?.anchorLinks?.h2)
+</script>
--- components/content/ProseH3.vue
+++ components/content/ProseH3.vue
@@ -1,7 +1,8 @@
 <template>
-   <h3 class="font-semibold text-xl mt-8 mb-4 text-gray-700 dark:text-white" :id="id">
-      <span class="mr-2 opacity-50">###</span>
-      <a v-if="id && generate" :href="`#${id}`">
+   <h3 class="group font-semibold text-xl mt-8 mb-4 text-gray-700 dark:text-white relative" :id="id">
+      <!-- <span class="mr-2 opacity-50">###</span> -->
+      <div class="ml-2 text-lg absolute -left-9 opacity-0 group-hover:opacity-15">H3</div>
+      <a v-if="id && generate" :href="`#${id}`" class="inline-block">
          <slot />
       </a>
       <slot v-else />
